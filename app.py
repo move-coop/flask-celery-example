033d4c78c2f0d9c1e1b6be2cc127ef6623fd4268
jburchard <jburchard@gmail.com>
2020-07-07 07:19:54 -0500

    Add.

--- app.py
+++ app.py
@@ -3,44 +3,64 @@ import random
 import time
 from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
-from flask_mail import Mail, Message
 from celery import Celery
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
 
-# Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
 
 # Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-
-# Initialize extensions
-mail = Mail(app)
+from parsons import Redshift
+rs = Redshift()
 
 # Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
 
-@celery.task
-def send_async_email(email_data):
-    """Background task to send an email with Flask-Mail."""
-    msg = Message(email_data['subject'],
-                  sender=app.config['MAIL_DEFAULT_SENDER'],
-                  recipients=[email_data['to']])
-    msg.body = email_data['body']
-    with app.app_context():
-        mail.send(msg)
+# @celery.task(bind=True)
+def query(sql):
+    """Background task to send sql query"""
+    return rs.query(sql)
 
+@app.route('/query', methods=['POST'])
+def async_query():
+
+    data = request.data
+    sql = 'select 1'
+    task = asyncquery.apply_async(args=[sql])
+    return jsonify({}), 202, {'Location': 'None'}
+
+@celery.task(bind=True)
+def asyncquery(self, sql):
+
+    return rs.query(sql)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'GET':
+        return render_template('index.html', email=session.get('email', ''))
+
+    if request.form['submit'] == 'Run':
+        sql = request.form['sql']
+        task = asyncquery.delay(sql)
+        flash(f'Submitted query. Task ID {task.id}, {task.kwargs}')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/longtask', methods=['POST'])
+def longtask():
+    task = long_task.apply_async()
+    return jsonify({}), 202, {'Location': url_for('taskstatus',
+                                                  task_id=task.id)}
 
 @celery.task(bind=True)
 def long_task(self):
@@ -61,39 +81,6 @@ def long_task(self):
         time.sleep(1)
     return {'current': 100, 'total': 100, 'status': 'Task completed!',
             'result': 42}
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html', email=session.get('email', ''))
-    email = request.form['email']
-    session['email'] = email
-
-    # send the email
-    email_data = {
-        'subject': 'Hello from Flask',
-        'to': email,
-        'body': 'This is a test email sent from a background Celery task.'
-    }
-    if request.form['submit'] == 'Send':
-        # send right away
-        send_async_email.delay(email_data)
-        flash('Sending email to {0}'.format(email))
-    else:
-        # send in one minute
-        send_async_email.apply_async(args=[email_data], countdown=60)
-        flash('An email will be sent to {0} in one minute'.format(email))
-
-    return redirect(url_for('index'))
-
-
-@app.route('/longtask', methods=['POST'])
-def longtask():
-    task = long_task.apply_async()
-    return jsonify({}), 202, {'Location': url_for('taskstatus',
-                                                  task_id=task.id)}
-
 
 @app.route('/status/<task_id>')
 def taskstatus(task_id):
